3c254256329216e42ea092498d259e7f126e991e
Karan Yang <ky393@cornell.edu>
2021-04-26 10:20:24 -0400

    hw5 update-4;

--- frontend/CoShop-app/src/Component/Navigation.js
+++ frontend/CoShop-app/src/Component/Navigation.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Login from './login'
-import Register from './Register'
+// import Register from './Register'
 
 export default function Navigation() {
   return (
     <Switch>
       <Route path="/" exact component={Login} />
       {/* <Route path="/invite" exact component={Invite} /> */}
-      <Route path="/register" component={Register} /> 
+      {/* <Route path="/register" component={Register} />  */}
     </Switch>
   )
 }
